9f39404250ab81e6859523029a2088e6da7c0204
HalakII <1017imiia@gmail.com>
2023-12-18 11:03:36 +0200

    start

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,17 @@
+import { ContactsListItem } from 'components/ContactsListItem/ContactsListItem';
+import css from './ContactsList.module.css';
+export const ContactsList = ({ filteredContacts, onDeleteContact }) => {
+  return (
+    <ul className={css.contactList}>
+      {filteredContacts.map(({ id, name, number }) => (
+        <ContactsListItem
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDeleteContact={onDeleteContact}
+        />
+      ))}
+    </ul>
+  );
+};
